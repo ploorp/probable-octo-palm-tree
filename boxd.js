2cccc5498adecd9093f17727e9a8e18e41891f56
ploorp <finnwither@gmail.com>
2025-04-09 00:45:38 -0400

    move boxd to antoehr file

---
+++ boxd.js
@@ -0,0 +1,84 @@
+import axios from 'axios';    
+
+async function boxd(msg) {
+  const args = msg.messageText.slice(5).trim().split(' ');
+
+  let username;
+  let rssUrl;
+  let response;
+
+  // if no arguments try to use the sender's username
+  if (!args[0].length) {
+    rssUrl = `https://letterboxd.com/${msg.senderUsername}/rss/`;
+    try {
+    response = await axios.get(rssUrl);
+    } catch (error) {
+    return client.say(
+      msg.channelName,
+      'format is !boxd <username>'
+    );
+    }
+    username = msg.senderUsername;
+  } else {
+    username = args[0].toLowerCase();
+    rssUrl = `https://letterboxd.com/${username}/rss/`;
+    try {
+    response = await axios.get(rssUrl);
+    } catch (error) {
+    return client.say(
+      msg.channelName,
+      'bad username'
+    );
+    }
+  }
+
+  const xmlData = response.data;
+  const linkMatch = xmlData.match(/<item>.*?<link>(.*?)<\/link>/s);
+
+  if (!linkMatch || !linkMatch[1].includes('/film/')) {
+    return client.say(
+    msg.channelName,
+    `${username} has not logged any movies`
+    );
+  }
+
+  const jsonUrl = linkMatch[1] + '/json';
+  let jsonResponse;
+
+  try {
+    jsonResponse = await axios.get(jsonUrl);
+  } catch (error) {
+    return client.say(
+    msg.channelName,
+    'error Reacting'
+    );
+  }
+
+  const jsonData = jsonResponse.data;
+
+  const movieTitle = jsonData.viewingable.name;
+  const reviewUrl = `https://letterboxd.com/${username}/film/${jsonData.viewingable.slug}`;
+  const movieUrl = `https://letterboxd.com/film/${jsonData.viewingable.slug}`;
+  const profileUrl = `https://letterboxd.com/${username}/`;
+  const dateLogged = jsonData.viewingDate;
+  // const dateLogged = jsonData.viewingDateStr;
+  const rating = jsonData.rating;
+  const rewatch = jsonData.rewatch ? 'rewatched' : 'watched';
+  const releaseDate = jsonData.viewingable.releaseYear;
+  const like = jsonData.liked;
+
+  let review = jsonData.reviewText;
+  if (review && review.length > 150) {
+    review = review.slice(0, 150) + '...';
+  }
+
+  const ratingText = `${rating || like ? 'rating:' + (rating ? ' ' + rating/2 + '/5' : '') + (like ? ' ❤️' : '') : ''}`;
+  const reviewText = `${review ? 'review: ' + review : ''}`;
+
+  const message = `${dateLogged} ${username} ${rewatch} ${movieTitle} (${releaseDate}) ${ratingText} ${reviewUrl}`;
+
+  return client.say(
+    msg.channelName,
+    message
+  );
+}
--- ploorp.js
+++ ploorp.js
@@ -1,6 +1,5 @@
 import { client } from './src/client.js';
-import config from './config.json' with { type: 'json' };
-import axios from 'axios';
+import boxd from './boxd.js';
 
 client.on('PRIVMSG', async (msg) => {
   /*
@@ -18,85 +17,19 @@ client.on('PRIVMSG', async (msg) => {
   }
 
   if (msg.messageText.startsWith('!boxd')) {
-    const args = msg.messageText.slice(5).trim().split(' ');
+    const message = await boxd(msg);
+    return client.say(msg.channelName, message);
+  }
 
-    let username;
-    let rssUrl;
-    let response;
+  if (msg.messageText.startsWith('A Raid') && msg.senderUsername === 'deepdankdungeonbot') {
+    return client.say(msg.channelName, '+ed');
+  }
 
-    // if no arguments try to use the sender's username
-    if (!args[0].length) {
-      rssUrl = `https://letterboxd.com/${msg.senderUsername}/rss/`;
-      try {
-        response = await axios.get(rssUrl);
-      } catch (error) {
-        return client.say(
-          msg.channelName,
-          'format is !boxd <username>'
-        );
-      }
-      username = msg.senderUsername;
-    } else {
-      username = args[0].toLowerCase();
-      rssUrl = `https://letterboxd.com/${username}/rss/`;
-      try {
-        response = await axios.get(rssUrl);
-      } catch (error) {
-        return client.say(
-          msg.channelName,
-          'bad username'
-        );
-      }
-    }
+  if (msg.messageText.includes('30 seconds') && msg.senderUsername === 'deepdankdungeonbot') {
+    return client.say(msg.channelName, '+join');
+  }
 
-    const xmlData = response.data;
-    const linkMatch = xmlData.match(/<item>.*?<link>(.*?)<\/link>/s);
-
-    if (!linkMatch || !linkMatch[1].includes('/film/')) {
-      return client.say(
-        msg.channelName,
-        `${username} has not logged any movies`
-      );
-    }
-
-    const jsonUrl = linkMatch[1] + '/json';
-    let jsonResponse;
-
-    try {
-      jsonResponse = await axios.get(jsonUrl);
-    } catch (error) {
-      return client.say(
-        msg.channelName,
-        'error Reacting'
-      );
-    }
-
-    const jsonData = jsonResponse.data;
-
-    const movieTitle = jsonData.viewingable.name;
-    const reviewUrl = `https://letterboxd.com/${username}/film/${jsonData.viewingable.slug}`;
-    const movieUrl = `https://letterboxd.com/film/${jsonData.viewingable.slug}`;
-    const profileUrl = `https://letterboxd.com/${username}/`;
-    const dateLogged = jsonData.viewingDate;
-    // const dateLogged = jsonData.viewingDateStr;
-    const rating = jsonData.rating;
-    const rewatch = jsonData.rewatch ? 'rewatched' : 'watched';
-    const releaseDate = jsonData.viewingable.releaseYear;
-    const like = jsonData.liked;
-
-    let review = jsonData.reviewText;
-    if (review && review.length > 150) {
-      review = review.slice(0, 150) + '...';
-    }
-
-    const ratingText = `${rating || like ? 'rating:' + (rating ? ' ' + rating/2 + '/5' : '') + (like ? ' ❤️' : '') : ''}`;
-    const reviewText = `${review ? 'review: ' + review : ''}`;
-
-    const message = `${dateLogged} ${username} ${rewatch} ${movieTitle} (${releaseDate}) ${ratingText} ${reviewUrl}`;
-
-    return client.say(
-      msg.channelName,
-      message
-    );
+  if (msg.messageText.includes('ploorp') && msg.messageText.includes('bot')) {
+    return client.say(msg.channelName, 'AA');
   }
 });
