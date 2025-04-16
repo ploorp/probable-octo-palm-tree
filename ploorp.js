13a76f1c4175b97a36fafaf77f0bb78794a0819f
ploorp <finnwither@gmail.com>
2025-04-15 23:29:55 -0400

    st

--- ploorp.js
+++ ploorp.js
@@ -17,18 +17,18 @@ client.on('PRIVMSG', async (msg) => {
   const roomState = client.roomStateTracker?.channelStates?.[msg.channelName];
   const botState = client.userStateTracker?.channelStates?.[msg.channelName];
 
-
-  if (!botState.isMod) {
-      if(roomState.emoteOnly || roomState.subOnly || roomState.followersOnlyDuration > -1) {
-        return;
-      }
+  if(!botState.isMod && (roomState.emoteOnly || roomState.subOnly || roomState.followersOnlyDuration > -1)) {
+    return;
   }
 
   if (msg.senderUserID == config.id) {
     return;
   }
 
-  if (msg.messageText.startsWith(config.prefix + 'ping')) {
+  var msgText = msg.messageText;
+  var args = msgText.trim().split(' ');
+
+  if (mstText.startsWith(config.prefix + 'ping')) {
     const uptime = Math.floor((Date.now() - startTime) / 1000);
     const days = Math.floor(uptime / 86400);
     const hours = Math.floor((uptime % 86400) / 3600);
@@ -44,60 +44,48 @@ client.on('PRIVMSG', async (msg) => {
     return client.say(msg.channelName, `@${msg.senderUsername}, catHop guptime: ${uptimeStr}`);
   }
 
-  if (msg.messageText.startsWith(config.prefix + 'boxd')) {
+  if (args[0] === config.prefix + 'boxd') {
     await boxd(msg);
     return;
   }
 
-  if (msg.messageText.startsWith(config.prefix + 'help')) {
+  if (args[0] === config.prefix + 'help') {
     const p = config.prefix;
-    return client.say(msg.channelName, `@${msg.senderUsername}, commands: ${p}ping, ${p}boxd <username>, ${p}help`);
-
+    return client.say(msg.channelName, `@${msg.senderUsername}, commands: ${p}ping, ${p}boxd <username>, ${p}follows <username>, ${p}help`);
   }
-
-  // if (msg.senderUserID === '883453487') {
-  //   if (msg.messageText.startsWith('A Raid')) {
-  //     await sleep(Math.random() * 10000 + 5000);
-  //     return client.say(msg.channelName, '+ed');
-  //   }
-
-  //   if (msg.messageText.includes('30 seconds')) {
-  //     await sleep(Math.random() * 10000 + 5000);
-  //     return client.say(msg.channelName, '+join');
-  //   }
-
-  //   return;
-  // }
-
-  // if (msg.messageText.includes('pl 00 rp')) {
-  //   return client.say(msg.channelName, '00');
-  // }
 
   // commands only whitelisted users can use 
   if (config.whitelist_channels.includes(msg.senderUsername)) {
-    if (msg.messageText.startsWith(config.prefix + 'part')) {
+    if (args[0] === config.prefix + 'part') {
       await client.part(msg.channelName);
     }
 
-    if (msg.messageText.startsWith(config.prefix + 'join')) {
-      const args = msg.messageText.slice(5).trim().split(' ');
-
-      if (args[0].length) {
-        await client.join(args[0]);
-        return client.say(msg.channelName, 'joined ' + args[0]);
+    if (args[0] === config.prefix + 'join') {
+      if (args[1].length) {
+        await client.join(args[1]);
+        return client.say(msg.channelName, 'joined ' + args[1]);
       }
     }
   }
 
-  if (msg.messageText.includes(config.username)) {
+  if (mstText.includes(config.username)) {
     return client.say(msg.channelName, msg.senderUsername + ' hi');
   }
 
-  if (msg.messageText.trim() === 'test') {
+  if (mstText.trim() === 'test') {
     return client.say(msg.channelName, 'A');
   }
 
-  if (msg.messageText.trim() === 'gup') {
+  if (mstText.trim() === 'gup') {
     return client.say(msg.channelName, 'gup');
+  }
+
+  if (args[0] === config.prefix + 'f' || args[0] === config.prefix + 'follows') {
+
+    if (!args[1].length) {
+      return client.say(msg.channelName,`https://tools.2807.eu/follows?user=${msg.senderUsername}`);
+    }
+
+    return client.say(msg.channelName, `https://tools.2807.eu/follows?user=${args[0]}`);
   }
 });
