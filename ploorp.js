29605961fbbf01e0bd98dcbd11d60a7c0e56c908
ploorp <finnwither@gmail.com>
2025-04-19 12:54:34 -0400

    there was a problem

--- ploorp.js
+++ ploorp.js
@@ -54,54 +54,8 @@ client.on('PRIVMSG', async (msg) => {
     setTimeout(() => cooldowns.delete(senderID), 5000);
   }
 
-  // COMMANDS
-  if (command === 'ping') {
-    const uptime = Math.floor((Date.now() - startTime) / 1000);
-    const days = Math.floor(uptime / 86400);
-    const hours = Math.floor((uptime % 86400) / 3600);
-    const minutes = Math.floor((uptime % 3600) / 60);
-    const seconds = uptime % 60;
 
-    let uptimeStr = 
-      days > 0 ? `${days}d ${hours}h` :
-      hours > 0 ? `${hours}h ${minutes}m` :
-      minutes > 0 ? `${minutes}m ${seconds}s` :
-      `${seconds}s`;
-
-    return client.say(msg.channelName, `@${msg.senderUsername}, catHop guptime: ${uptimeStr}`);
-  }
-
-  if (command === 'boxd') {
-    await boxd(msg);
-    return;
-  }
-
-  if (command === 'help') {
-    const p = config.prefix;
-    return client.say(msg.channelName, `@${msg.senderUsername}, commands: ${p}ping, ${p}boxd <username>, ${p}follows <username>, ${p}help`);
-  }
-
-  // Commands only whitelisted users can use
-  if (config.whitelist_channels.includes(msg.senderUsername)) {
-    if (command === 'part') {
-      await client.part(msg.channelName);
-      return;
-    }
-
-    if (command === 'join') {
-      if (args[1]?.length) {
-        try {
-          await client.join(args[1].toLowerCase());
-        } catch (error) {
-          return client.say(msg.channelName, 'error joining ' + args[1]);
-        }
-        return client.say(msg.channelName, 'joined ' + args[1]);
-      }
-
-      return;
-    }
-  }
-
+  // STUFF THATS NOT REALLY A COMMAND
   if (msgText.includes(config.username)) {
     return client.say(msg.channelName, msg.senderUsername + ' hi');
   }
@@ -114,8 +68,62 @@ client.on('PRIVMSG', async (msg) => {
     return client.say(msg.channelName, 'gup');
   }
 
-  if (command === 'f' || command === 'follows') {
-    const username = args[1] ? args[1] : '';
-    return client.say(msg.channelName, `@${msg.senderUsername}, https://tools.2807.eu/follows?user=${username}`);
+  
+  // COMMANDS
+  if (msgText.startsWith(config.prefix)) {
+    const args = msgText.split(' ');
+    const command = args[0].slice(config.prefix.length);
+
+    if (command === 'ping') {
+      const uptime = Math.floor((Date.now() - startTime) / 1000);
+      const days = Math.floor(uptime / 86400);
+      const hours = Math.floor((uptime % 86400) / 3600);
+      const minutes = Math.floor((uptime % 3600) / 60);
+      const seconds = uptime % 60;
+
+      let uptimeStr = 
+        days > 0 ? `${days}d ${hours}h` :
+        hours > 0 ? `${hours}h ${minutes}m` :
+        minutes > 0 ? `${minutes}m ${seconds}s` :
+        `${seconds}s`;
+
+      return client.say(msg.channelName, `@${msg.senderUsername}, catHop guptime: ${uptimeStr}`);
+    }
+
+    if (command === 'boxd') {
+      await boxd(msg);
+      return;
+    }
+
+    if (command === 'help') {
+      const p = config.prefix;
+      return client.say(msg.channelName, `@${msg.senderUsername}, commands: ${p}ping, ${p}boxd <username>, ${p}follows <username>, ${p}help`);
+    }
+
+    if (command === 'f' || command === 'follows') {
+      const username = args[1] ? args[1] : '';
+      return client.say(msg.channelName, `@${msg.senderUsername}, https://tools.2807.eu/follows?user=${username}`);
+    }
+
+    // Commands only whitelisted users can use
+    if (config.whitelist_channels.includes(msg.senderUsername)) {
+      if (command === 'part') {
+        await client.part(msg.channelName);
+        return;
+      }
+
+      if (command === 'join') {
+        if (args[1]?.length) {
+          try {
+            await client.join(args[1].toLowerCase());
+          } catch (error) {
+            return client.say(msg.channelName, 'error joining ' + args[1]);
+          }
+          return client.say(msg.channelName, 'joined ' + args[1]);
+        }
+
+        return;
+      }
+    }
   }
 });
