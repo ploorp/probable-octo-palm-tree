bf37ae0ceebf7632da90bb236c11cf88cba9a60f
ploorp <finnwither@gmail.com>
2025-04-19 00:18:02 -0400

    stupid fuck

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,10 @@
 import fs from 'fs';
-import path from 'path';
 
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const logToFile = (message, file) => {
   const timestamp = new Date().toISOString();
   const logMessage = `[${timestamp}] ${message}\n`;
-  const logFile = path.join(__dirname, file);
 
-  fs.appendFile(logFile, logMessage);
+  fs.appendFile(file, logMessage);
 }
