11ff353be4b1b06d7ddb011d63e5aa2447d32ce9
ploorp <finnwither@gmail.com>
2025-04-19 01:26:23 -0400

    z

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 export const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const logToFile = (message, file) => {
-  const timestamp = now.toLocaleString('en-US', {hour12: false});
+  const timestamp = new Date().toLocaleString('en-US', {hour12: false});
   const logMessage = `[${timestamp}] ${message}\n`;
 
   fs.appendFile(file, logMessage, err => {
