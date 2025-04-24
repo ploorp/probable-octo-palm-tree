ad83a464b547144296de8c1cabd62d0033a60702
ploorp <finnwither@gmail.com>
2025-04-23 22:37:31 -0400

    testing new command

---
+++ connections.js
@@ -0,0 +1,63 @@
+import { client } from './src/client.js';
+import axios from 'axios';    
+
+export default async function connections(msg) {
+  const args = msg.messageText.slice(5).trim().split(' ');
+
+  let username;
+  let response;
+
+  const endpoint = "https://api.potat.app/users/"
+
+  // if no arguments try to use the sender's username
+  if (!args[0].length) {
+    username = msg.senderUsername;
+
+    try {
+      response = await axios.get(endpoint + username);
+    } catch (error) {
+      return client.say(msg.channelName, `@${msg.senderUsername}, you were never seen by potatbotat Reacting`);
+    }
+  } else {
+    username = args[0].toLowerCase();
+
+    if (!/^[a-z0-9_]+$/.test(username)) {
+      return client.say(msg.channelName, `@${msg.senderUsername}, bad username`);
+    }
+
+    
+    try {
+      response = await axios.get(endpoint + username);
+    } catch (error) {
+      return client.say(msg.channelName, `@${msg.senderUsername}, this user has never been seen by potatbotat Reacting`);
+    }
+  }
+
+  const connections = response.data["data"][0]["connections"];
+  let spotify;
+  let lastfm;
+
+  for (let i = 0; i < connections.length; i++) {
+    const platform = connections[i]["platform"];
+
+    if (platform === "SPOTIFY") {
+      spotify = connections[i]["id"];
+    }
+
+    if (platform === "LASTFM") {
+      lastfm = connections[i]["id"];
+    }
+  }
+
+  if (!spotify && !lastfm) {
+    return client.say(msg.channelName, `@${msg.senderUsername}, ${username} hasn't connected any interesting accounts`);
+  }
+
+  const spotifyUrl = `${spotify ? `https://open.spotify.com/user/${spotify}` : ''}`;
+  const lastfmUrl = `${lastfm ? `https://www.last.fm/${lastfm}` : ''}`;
+
+  const message = `${username}'s connected accounts: ${spotifyUrl} ${lastfmUrl}`;
+
+  return client.say(msg.channelName, `@${msg.senderUsername}, ${message}`);
+}
+ 
