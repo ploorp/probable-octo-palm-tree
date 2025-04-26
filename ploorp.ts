b05a1dfd969517448980f46351dc77714540a28f
Finn <36242395+ploorp@users.noreply.github.com>
2025-04-26 03:30:21 -0400

    op

--- ploorp.ts
+++ ploorp.ts
@@ -47,10 +47,10 @@ client.on('PRIVMSG', async (msg: PrivmsgMessage) => {
   var msgText;
 
   if (msg.replyParentMessageBody) {
-    msgText = msg.replyParentMessageBody.split(' ').slice(1).join(' ').trim();
-  } else {
-    msgText = msg.messageText.trim();
+    console.log('replyParentMessageBody: ', msg.replyParentMessageBody);
   }
+
+  msgText = msg.messageText.trim();
 
   const args = msgText.split(' ');
   const command = args[0].slice(config.prefix.length);
