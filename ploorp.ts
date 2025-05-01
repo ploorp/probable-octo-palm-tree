4e310dcb7a470cd169aaf0e732e2f9281384088e
ploorp <finnwither@gmail.com>
2025-05-01 00:22:49 -0400

    ok

--- ploorp.ts
+++ ploorp.ts
@@ -122,8 +122,7 @@ client.on('PRIVMSG', async (msg: PrivmsgMessage) => {
         case 'ban':
           const channels = config.ban_list;
           for (const channel of channels) {
-            //await client.say(channel, `/me ${args[1]}`);
-            await client.sendRaw(`PRIVMSG #${channel} :${args[1]}`);
+            await client.sendRaw(`PRIVMSG #${channel} :/me${args[1]}`);
         }
         return;
     }
