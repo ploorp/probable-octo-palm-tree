625038ef1a504247de54fad10953c64d12d624ee
ploorp <finnwither@gmail.com>
2025-05-03 01:03:39 -0400

    bro

--- src/helix.ts
+++ src/helix.ts
@@ -1,6 +1,6 @@
 import config from '../config.json' with { type: 'json' };
 import axios from 'axios';
-import { timeLog } from './utils';
+import { timeLog } from './utils.js';
 
 const BAN_API = 'https://api.twitch.tv/helix/moderation/bans';
 const clientId = config.helix.client_id;
