01e06ab8b6b143c167075d40fa5fca78416c7261
ploorp <finnwither@gmail.com>
2025-05-07 15:33:42 -0400

    asdf

--- ploorp.ts
+++ ploorp.ts
@@ -29,7 +29,7 @@ client.on('JOIN', async (msg) => {
   const banChannels = config.ban_list;
   const banPattern = new RegExp(config.ban_pattern, "i");
 
-  console.log(joinedUser, "joined", msg.channelName);
+  //console.log(joinedUser, "joined", msg.channelName);
 
   if (userID === config.id || config.channels.includes(joinedUser)) {
     return;
@@ -50,9 +50,8 @@ client.on('JOIN', async (msg) => {
   const isPfpChanged = !await isPfpDefault(userInfo);
 
   if (isNewChatter) {
-    console.log('isNewChatter', joinedUser);
+    timeLog('new chatter' + joinedUser);
     if (banPattern.test(joinedUser)) {
-      console.log('regex', joinedUser);
       for (const channel of banChannels) {
         chatBan(userID, channel, 'band');
         client.say(channel, `@${msg.channelName}, banned ${joinedUser} use %undo to unban`);
