6b46cc6c486505cc7c3d578a4a292aa332e90940
ploorp <finnwither@gmail.com>
2025-11-02 02:01:41 -0500

    a

--- src/commands/newname.ts
+++ src/commands/newname.ts
@@ -2,10 +2,11 @@ import { PrivmsgMessage } from '@mastondzn/dank-twitch-irc';
 import { client, saySafe } from '../client.js';
 import { getUsername, whisperUser } from '../helix.js';
 import { timeLog } from '../utils.js';
+import { getPrefix } from '../db/dbManager.js';
 
 export default async function newname(msg: PrivmsgMessage, args: string[]) {
   if (!args[1]) {
-    return saySafe(msg.channelName, `@${msg.senderUsername}, usage: !newname <username>`);
+    return saySafe(msg.channelName, `@${msg.senderUsername}, usage: ${getPrefix(msg.channelID)} newname <username>`);
   }
 
   const username = args[1].toLowerCase().replace(/^@/, '');
