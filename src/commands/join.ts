3b53e203ae366517d30bc4934e34b7fed60321a4
Finn <36242395+ploorp@users.noreply.github.com>
2026-03-16 00:15:09 -0400

    better error

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -2,6 +2,7 @@ import { PrivmsgMessage } from '@mastondzn/dank-twitch-irc';
 import { client, saySafe } from '../client.js';
 import { partChannel, isWhitelisted, addChannel } from '../db/dbManager.js';
 import { getUserId } from '../api/helix.js';
+import { timeLog } from '../utils.js';
 
 export default async function join(msg: PrivmsgMessage, args: string[], command: string = "join") {
 
@@ -11,6 +12,10 @@ export default async function join(msg: PrivmsgMessage, args: string[], command:
         await client.join(msg.senderUsername);
         addChannel(msg.senderUserID);
       } catch (error) {
+        timeLog(`Error joining ${msg.senderUsername}: ${error}`);
+        if (String(error).includes("Timed out")) {
+             return saySafe(msg.channelName, 'error joining ' + msg.senderUsername + ' (timed out maybe banned?)', msg.messageID);
+        }
         return saySafe(msg.channelName, 'error joining ' + msg.senderUsername, msg.messageID);
       }
       return saySafe(msg.channelName, 'joined ' + msg.senderUsername, msg.messageID);
@@ -20,30 +25,36 @@ export default async function join(msg: PrivmsgMessage, args: string[], command:
         partChannel(msg.senderUserID);
         return saySafe(msg.channelName, 'leaving ' + msg.senderUsername, msg.messageID);
       } catch (error) {
+        timeLog(`Error parting ${msg.senderUsername}: ${error}`);
         return saySafe(msg.channelName, 'error Reacting', msg.messageID);
       }
     }
   } else {
     if (command === 'join') {
-      if (isWhitelisted(msg.senderUserID) || args[1].toLowerCase() === msg.senderUserID) {
+      if (isWhitelisted(msg.senderUserID) || args[1].toLowerCase() === msg.senderUsername) {
         try {
           await client.join(args[1].toLowerCase());
           const joinId = await getUserId(args[1].toLowerCase()) as string;
           addChannel(joinId);
         } catch (error) {
+           timeLog(`Error joining ${args[1]}: ${error}`);
+            if (String(error).includes("Timed out")) {
+                 return saySafe(msg.channelName, 'error joining ' + args[1] + ' (timed out maybe banned?)', msg.messageID);
+            }
           return saySafe(msg.channelName, 'error joining ' + args[1], msg.messageID);
         }
         return saySafe(msg.channelName, 'joined ' + args[1], msg.messageID);
       } else {
         return saySafe(msg.channelName, `you can only join your own channel`, msg.messageID);
       }
-    } else if (isWhitelisted(msg.senderUserID) || args[1].toLowerCase() === msg.senderUserID) {
+    } else if (isWhitelisted(msg.senderUserID) || args[1].toLowerCase() === msg.senderUsername) {
       try {
         saySafe(msg.channelName, 'leaving ' + args[1], msg.messageID);
         await client.part(args[1].toLowerCase());
         const partId = await getUserId(args[1].toLowerCase()) as string;
         return partChannel(partId);
       } catch (error) {
+        timeLog(`Error parting ${args[1]}: ${error}`);
         return saySafe(msg.channelName, 'error Reacting', msg.messageID);
       }
     } else {
